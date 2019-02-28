9dc1f7bb2d0aa206051abfeee2d5832bf3baef39
cwu155 <cwu17@stuy.edu>
2019-02-28 14:24:29 -0500

    Added constructor?

--- Maze.java
+++ Maze.java
@@ -9,10 +9,10 @@ public class Maze{
   private int rows, cols;
 
   public Maze(String filename) throws FileNotFoundException{
+
+      try {
       rows = 0;
       File text = new File(filename);
-
-      //inf stands for the input file
       Scanner inf = new Scanner(text);
 
       while(inf.hasNextLine()){
@@ -21,8 +21,38 @@ public class Maze{
 
           if (rows == 1){
                columns = line.toCharArray();
+               cols = columns.length;
           }
-         }
+        }
 
+        char[][] maze = new char[rows][cols];
+
+      } catch (FileNotFoundException ex){
+        System.out.println("Yikes");
+      }
+
+    }
+
+    private void wait(int millis){
+           try {
+               Thread.sleep(millis);
+           }
+           catch (InterruptedException e) {
+           }
        }
+
+      public void setAnimate(boolean b){
+          animate = b;
+      }
+
+      public void clearTerminal(){
+          //erase terminal, go to top left of screen.
+          System.out.println("\033[2J\033[1;1H");
+      }
+
+
+    public static void main(String[] args) throws FileNotFoundException{
+      Maze a = new Maze("Maze1.txt");
+      System.out.println(a);
+    }
   }
